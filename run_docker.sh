d7c1e2d9e5098fa4885f8414fa0c1f78da9b06c1
Lars Rasmusson <Lars.Rasmusson@ri.se>
2021-01-11 11:35:38 +0000

    cleaned up Docker instructions

--- run_docker.sh
+++
@@ -1,2 +0,0 @@
-docker run --device /dev/sgx/provision --device /dev/sgx/enclave --rm -ti asclepios \
-   sh -c "cd /root/asclepios-teep; . /root/.bashrc; (python -c 'import simple; simple.start_server()' & sleep 1; python -c 'import simple; simple.sealingtest()' ; wait)"
